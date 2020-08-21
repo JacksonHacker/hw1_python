5784d7691dedd3a5044ade6cd027dbb3b3912128
mithun <mithunpaul08@gmail.com>
2020-08-21 01:58:54 -0700

    adding code stub for python

---
+++ src/test/python/edu/arizona/cs/testq5.py
@@ -0,0 +1,11 @@
+# test_with_unittest.py
+
+from unittest import TestCase
+from src.main.python.edu.arizona.cs.invertedindex import InvertedIndex
+
+class TryTesting(TestCase):
+    def test_always_passes(self):
+        assert True
+
+    def test_always_fails(self):
+        assert False
